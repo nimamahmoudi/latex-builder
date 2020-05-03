92cf905c5966983fb2362a17ca5b643ed60191bb
Nima Mahmoudi <nima_mahmoudi@live.com>
2020-05-03 17:36:01 -0400

    added multer and form auto submit functionality

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -16,7 +16,7 @@ $("#select-file-text").on('drag dragstart dragend dragover dragenter dragleave d
 })
   .on('click', (e) => {
     e.preventDefault();
-    $fileInput.trigger('click'); 
+    $fileInput.trigger('click');
   })
 
 // for drop behaviour
@@ -35,5 +35,50 @@ $form.on('drag dragstart dragend dragover dragenter dragleave drop', function (e
   })
   .on('drop', function (ev) {
     droppedFiles = ev.originalEvent.dataTransfer.files;
-    console.log(droppedFiles[0].name)
+    $form.trigger('submit');
   });
+
+$fileInput.on('change', function (e) {
+  $form.trigger('submit');
+});
+
+$form.on('submit', function (e) {
+  if ($form.hasClass('is-uploading')) return false;
+
+  $form.addClass('is-uploading').removeClass('is-error');
+
+  var ajaxData = new FormData($form.get(0));
+
+  if (droppedFiles) {
+    $.each(droppedFiles, function (i, file) {
+      ajaxData.append($fileInput.attr('name'), file);
+    });
+
+    console.log(droppedFiles)
+    $form.find('input[type="file"]').prop('files', droppedFiles);
+  }
+
+  console.log('Ajax Upload!')
+  e.preventDefault()
+
+  // $.ajax({
+  //   url: $form.attr('action'),
+  //   type: $form.attr('method'),
+  //   data: ajaxData,
+  //   dataType: 'json',
+  //   cache: false,
+  //   contentType: false,
+  //   processData: false,
+  //   complete: function () {
+  //     $form.removeClass('is-uploading');
+  //   },
+  //   success: function (data) {
+  //     $form.addClass(data.success == true ? 'is-success' : 'is-error');
+  //     if (!data.success) $errorMsg.text(data.error);
+  //   },
+  //   error: function () {
+  //     // Log the error, show an alert, whatever works for you
+  //   }
+  // });
+  
+});
